43ab3a51673ab988feba133a0e9be7e8f82800a0
21EG106B07 <21eg106b07@anurag.edu.in>
2023-10-03 09:47:57 +0530

    push

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.all('*',(req,res)=>{
+  res.status(404).render('./pages/Oops.ejs')
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
